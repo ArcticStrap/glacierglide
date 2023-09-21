3efa11f973049532a256906fe9275dcc2e975b9b
ChaosIsFramecode <framecodechaos@gmail.com>
2023-09-21 19:03:53 -0400

    Basic routing

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	log.Println("Welcome to horinezumi")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to 堀ネズミ!"))
+	})
+	http.ListenAndServe(":8080", r)
 }
