5f28896dfc005b6e6baddfa62259a7daa2090bc1
ChaosIsFramecode <framecodechaos@gmail.com>
2024-02-14 22:23:52 +0100

    Tokenize headers

--- mprender/markdown/engine.go
+++ mprender/markdown/engine.go
@@ -8,6 +8,11 @@ type TokenType int
 
 const (
 	Header1 TokenType = iota
+  Header2
+  Header3
+  Header4
+  Header5
+  Header6
 	Paragraph
 )
 
@@ -19,13 +24,40 @@ type Token struct {
 func Tokenize(content string) []Token {
 	var tokens []Token
 
-	headerRegex := regexp.MustCompile(`^#\s+(.*)$`)
+	headerRegex := regexp.MustCompile(`^(#+)\s+(.*)$`)
 
 	lines := regexp.MustCompile(`\r\n|\n|\r`).Split(content, -1)
 	for _, line := range lines {
-		if headerMatch := headerRegex.FindStringSubmatch(line); len(headerMatch) == 2 {
-			tokens = append(tokens, Token{Type: Header1, Value: headerMatch[1]})
-		}
+		if headerMatch := headerRegex.FindStringSubmatch(line); len(headerMatch) == 3 {
+      headerLevel := len(headerMatch[1])
+      switch headerLevel {
+      case 1:
+        tokens = append(tokens, Token{Type: Header1, Value: headerMatch[2]})
+        break
+      case 2:
+        tokens = append(tokens, Token{Type: Header2, Value: headerMatch[2]})
+        break
+      case 3:
+        tokens = append(tokens, Token{Type: Header3, Value: headerMatch[2]})
+        break
+      case 4:
+        tokens = append(tokens, Token{Type: Header4, Value: headerMatch[2]})
+        break
+      case 5:
+        tokens = append(tokens, Token{Type: Header5, Value: headerMatch[2]})
+        break
+      case 6:
+        tokens = append(tokens, Token{Type: Header6, Value: headerMatch[2]})
+        break
+      default:
+        tokens = append(tokens,Token{Type:Paragraph, Value: line})
+        break
+      }
+    } else {
+      // Assume its a paragraph
+      tokens = append(tokens,Token{Type:Paragraph, Value: line})
+    }
+
 	}
 
 	return tokens
--- mprender/markdown/markdown_test.go
+++ mprender/markdown/markdown_test.go
@@ -3,7 +3,14 @@ package markdown
 import "testing"
 
 func TestMarkdownEngine(t *testing.T) {
-  if len(Tokenize("# test")) != 1 {
-    t.Fatal("Tokenizer did not detect header syntax")
-  }
+	test_case := "# test 1\n## test 2"
+
+	tokens := Tokenize(test_case)
+	if len(tokens) != 2 {
+		t.Fatalf("Tokenizer did not detect header syntax: %d != 2", len(tokens))
+	} else {
+		for i, v := range tokens {
+			t.Logf("TOKEN %d: TYPE: %d, VALUE: %s", i, v.Type, v.Value)
+		}
+	}
 }
