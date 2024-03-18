1421180313b9ee9e83b4625b23fda53be3d59f83
ChaosIsFramecode <framecodechaos@gmail.com>
2024-03-18 01:13:35 +0100

    Simplify appsignal file

--- appsignals/appsignals.go
+++ appsignals/appsignals.go
@@ -9,4 +9,83 @@ package appsignals
  *  onCreateAccount(username)
  */
 
+import "sync"
+
 type SignalHandler func(interface{})
+
+type SignalConnector struct {
+	connected map[string][]SignalHandler
+	waitMap   map[string][]chan struct{}
+	mu        sync.Mutex
+}
+
+type SignalConnection struct {
+	Disconnect func()
+}
+
+func NewSignalConnector() *SignalConnector {
+	return &SignalConnector{
+		connected: make(map[string][]SignalHandler),
+		waitMap:   make(map[string][]chan struct{}),
+	}
+}
+
+func (sc *SignalConnector) Connect(signalType string, handler SignalHandler) SignalConnection {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+	sc.connected[signalType] = append(sc.connected[signalType], handler)
+
+	return SignalConnection{
+		Disconnect: func() {
+			sc.Disconnect(signalType, &handler)
+		},
+	}
+}
+
+func (sc *SignalConnector) Disconnect(signalType string, handler *SignalHandler) {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+	if handlers, ok := sc.connected[signalType]; ok {
+		for i, h := range handlers {
+			if &h == handler {
+				sc.connected[signalType] = append(handlers[:i], handlers[i+1:]...)
+				break
+			}
+		}
+	}
+}
+
+func (sc *SignalConnector) Fire(signalType string, signalData interface{}) {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+
+	// Call handler functions
+	for _, handler := range sc.connected[signalType] {
+		go handler(signalData)
+	}
+
+	// Signal wait channels
+	for _, ch := range sc.waitMap[signalType] {
+		close(ch)
+		delete(sc.waitMap, signalType)
+	}
+}
+
+// UNSTABLE: DO NOT USE UNTIL FURTHER TESTING IS CONDUCTED
+func (sc *SignalConnector) Wait(signalType string) {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+
+	// Create wait channel
+	ch := make(chan struct{})
+	sc.waitMap[signalType] = append(sc.waitMap[signalType], ch)
+
+	// Unlock to allow fire to execute
+	sc.mu.Unlock()
+	// Wait until fired
+	<-ch
+
+	// Relock after waiting
+	sc.mu.Lock()
+
+}
