8b4c2afb352fb4366dd71aa0a86b63829f25d146
ChaosIsFramecode <framecodechaos@gmail.com>
2024-03-23 23:04:09 +0100

    Fix time

--- middleware/logging.go
+++ middleware/logging.go
@@ -27,6 +27,6 @@ func Logging(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(wrapped, r)
-		fmt.Fprintf(os.Stdout, "%s \"%s %s %s\" from %s - %d in %s\n", start.Format("2006-01-02 01:16:50"), r.Method, r.URL.Path, r.Proto, r.RemoteAddr, wrapped.statusCode, time.Since(start))
+		fmt.Fprintf(os.Stdout, "%s \"%s %s %s\" from %s - %d in %s\n", start.Format("2006-01-02 15:04:05"), r.Method, r.URL.Path, r.Proto, r.RemoteAddr, wrapped.statusCode, time.Since(start))
 	})
 }
